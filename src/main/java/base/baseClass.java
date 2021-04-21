7d7bc1948f3c5c8f094f0d6cceac64f7787da308
Adnan <aliadnan38@gmail.oc>
2021-04-21 16:24:24 +0500

    changes in the pom.xml and base class

--- src/main/java/base/baseClass.java
+++ src/main/java/base/baseClass.java
@@ -2,7 +2,6 @@ package base;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class baseClass {
     public  static WebDriver driver;
