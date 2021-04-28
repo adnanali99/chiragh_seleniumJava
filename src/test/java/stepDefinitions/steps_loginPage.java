45acc4f402a9eed4590bc7e49122704d1fd38c61
Adnan <aliadnan38@gmail.oc>
2021-04-28 16:01:32 +0500

    updated path for local chromedriver

--- src/test/java/stepDefinitions/steps_loginPage.java
+++ src/test/java/stepDefinitions/steps_loginPage.java
@@ -32,9 +32,10 @@ public class steps_loginPage extends BaseClass {
 
 //        String browser = System.getProperty("browser");
 
-       //String browser = System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "//Drivers/chromedriver");
+
         System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY,"true");
-        String browser = System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome-stable");
+        String browser = System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "//Drivers/chromedriver");
+        // String browser = System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome-stable");
         logger= Logger.getLogger("ChiraghCucumber");
      PropertyConfigurator.configure("log4j.properties");
         if (browser == null) {
